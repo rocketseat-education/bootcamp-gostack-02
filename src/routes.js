d15da0267533e51d53f97fefe29c5ac123a9775e
Diego Fernandes <diego.schell.f@gmail.com>
2019-06-04 15:11:34 -0300

    10 - Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rocketseat.com.br',
+    password_hash: '1238712387',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
